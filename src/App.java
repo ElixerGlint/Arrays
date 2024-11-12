9d12218b1a42f539251e61de12a161dcc136f5ad
Elixer <everett.rumery@gmail.com>
2024-11-12 14:06:04 -0500

    asgasg

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int[] arr; //array of intergers
+        arr = new int[30];
+        System.out.println(arr[0]);
+        System.out.println(arr);
+
+        for (int i = 0; i < arr.length; i++) { //arrays are immutable length wise, length is a final int
+            arr[i] = (int)(Math.random()*365);
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(findSame(arr));
+    }
+
+    public static int findSame(int[] brr) {
+        brr = brr.clone(); //means its no longer editing the memory of the OG
+        Arrays.sort(brr);
+        for(int i = 1; i < brr.length; i++) {
+                if(brr[i] == brr[i-1]) {
+                    return brr[i];
+                }
+        }
+    return -1;
     }
 }
